08c610d0098515a8a19c4176bfa1c13d5e0b08e3
xtlog <lecil@163.com>
2016-07-14 08:11:43 +0800

    4444

--- credit/ServiceTableViewController.swift
+++ credit/ServiceTableViewController.swift
@@ -57,7 +57,6 @@ class ServiceTableViewController: UITableViewController {
 
         return cell
     }
-
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
